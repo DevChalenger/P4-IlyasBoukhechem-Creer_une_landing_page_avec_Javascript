58a7f5147281be21bea10658f327c7924aa837b7
Ilyas Boukhechem <ilyas.boukhechem@gmx.fr>
2022-02-19 21:12:23 +0100

    add regex for birthdate and tournaments

--- modal.js
+++ modal.js
@@ -28,24 +28,36 @@ form.addEventListener("submit", (event) => {
 });
 function validate() {
   const field = {
-    firstName: document.getElementById("first").value,
-    lastName: document.getElementById("last").value,
-    eMail: document.getElementById("email").value,
-    birthDate: document.getElementById("birthdate").value,
-    quantityOfTournaments: document.getElementById("quantity").value,
-    location1: document.getElementById("location1").value,
-    location2: document.getElementById("location2").value,
-    location3: document.getElementById("location3").value,
-    location4: document.getElementById("location4").value,
-    location5: document.getElementById("location5").value,
-    location6: document.getElementById("location6").value,
+    firstName: {
+      value: document.getElementById("first").value,
+      errorMessage:
+        "Veuillez remplir le champ avec un prénom valide pour pouvoir continuer",
+    },
+    lastName: {
+      value: document.getElementById("last").value,
+      errorMessage:
+        "Veuillez remplir le champ avec un nom valide pour pouvoir continuer",
+    },
+    eMail: {
+      value: document.getElementById("email").value,
+      errorMessage:
+        "Veuillez remplir le champ avec un email valide pour pouvoir continuer",
+    },
+    birthDate: { value: document.getElementById("birthdate").value },
+    quantityOfTournaments: { value: document.getElementById("quantity").value },
+    location1: { value: document.getElementById("location1").value },
+    location2: { value: document.getElementById("location2").value },
+    location3: { value: document.getElementById("location3").value },
+    location4: { value: document.getElementById("location4").value },
+    location5: { value: document.getElementById("location5").value },
+    location6: { value: document.getElementById("location6").value },
   };
 
   const controleInput = (value) => {
     return /^[A-Za-z]{1,20}$/.test(value);
   };
   function controlFirstName() {
-    if (controleInput(field.firstName)) {
+    if (controleInput(field.firstName.value)) {
       return true;
     } else {
       alert(
@@ -55,7 +67,7 @@ function validate() {
     }
   }
   function controlLastName() {
-    if (controleInput(field.lastName)) {
+    if (controleInput(field.lastName.value)) {
       return true;
     } else {
       alert(
@@ -66,8 +78,10 @@ function validate() {
   }
   function controlEmail() {
     if (
-      /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(field.eMail) &&
-      field.eMail.length > 1
+      /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(
+        field.eMail.value
+      ) &&
+      field.eMail.value.length > 1
     ) {
       return true;
     } else {
@@ -78,10 +92,23 @@ function validate() {
     }
   }
   function controlBirthDate() {
-    if (field.birthDate.length < 1) {
+    if (
+      /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/.test(
+        field.birthDate.value
+      )
+    ) {
+      return true;
+    } else {
       alert(
         "Veuillez remplir le champ avec une date de naissance valide pour pouvoir continuer"
       );
+      return false;
+    }
+  }
+  function controlQuantityOfTournaments() {
+    if (/^[0-9]+$^/.test(field.quantityOfTournaments.value)) {
+      return true;
+    } else {
       return false;
     }
   }
@@ -90,4 +117,5 @@ function validate() {
   controlLastName();
   controlEmail();
   controlBirthDate();
+  controlQuantityOfTournaments();
 }
